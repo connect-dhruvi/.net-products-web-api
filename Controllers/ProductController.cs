bf0f9f384dff53b8ca9ecf216f89dde44d241951
LAPTOP-HEI0V66S\conne <connect.dhruvi@gmail.com>
2020-12-29 16:32:22 -0500

    Inital Commit

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace web_api.Controllers
+{
+    public class ProductController : Controller
+    {
+        [Route("[controller]")]
+        [HttpGet]
+        public string GetProdcuts() =>
+            "Hello world";
+    }
+}
---
+++ Models/Order.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace web_api.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public int USerID { get; set; }
+        [JsonIgnore]
+
+        public virtual User User { get; set; }
+
+        public virtual List<Product> Products { get; set; }
+
+    }
+}
